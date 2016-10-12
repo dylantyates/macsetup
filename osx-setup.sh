021cf5da7706eca0b71643dcc3b0d46cc546850e
Dylan Yates <dwreckfoo@users.noreply.github.com>
2016-10-11 20:13:58 -0500

    Add defaults

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+
+defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
+defaults write com.apple.finder CreateDesktop -bool false && killall Finder; # take hidden files off of desktop

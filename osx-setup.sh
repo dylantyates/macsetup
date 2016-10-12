b9e8236b61ff8fcc45efe5f2f7fcd318154a0449
Dylan Yates <dwreckfoo@users.noreply.github.com>
2016-10-11 20:16:28 -0500

    Add .bash_profile

--- osx-setup.sh
+++ osx-setup.sh
@@ -3,3 +3,7 @@
 defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
 defaults write com.apple.finder CreateDesktop -bool false && killall Finder; # take hidden files off of desktop
+
+echo "Setting up ~/.bash_profile...";
+touch ~/.bash_profile;
+echo "export PS1=\"\w $ \";" >> ~/.bash_profile;

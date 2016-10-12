303c604e4ffb54e672491a5077f007208de5908e
Dylan Yates <dwreckfoo@users.noreply.github.com>
2016-10-11 20:18:33 -0500

    Add xcode command line tools

--- osx-setup.sh
+++ osx-setup.sh
@@ -7,3 +7,13 @@ defaults write com.apple.finder CreateDesktop -bool false && killall Finder; # t
 echo "Setting up ~/.bash_profile...";
 touch ~/.bash_profile;
 echo "export PS1=\"\w $ \";" >> ~/.bash_profile;
+
+# install Xcode Command Line Tools
+# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
